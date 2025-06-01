d0b554a88d8655db7940c55a0f5ec527b345cdcb
Jean2232 <jeannmc010@gmail.com>
2025-06-01 14:23:43 -0300

    lucky temp

--- plugins/fun-lucky.js
+++ plugins/fun-lucky.js
@@ -1,6 +1,33 @@
+import fs from 'fs'
+
+let file = '../tmp/sorte.json'
+let sorteData = {}
+
+if (fs.existsSync(file)) {
+    sorteData = JSON.parse(fs.readFileSync(file))
+}
+
 let handler = async (m, { conn }) => {
-    let sorte = Math.floor(Math.random() * 101) // 0 a 100%
+    const user = m.sender
+
+    const now = Date.now()
+    const data = sorteData[user]
+
+    if (data && now - data.timestamp < 86400000) {
+        let restante = clockString(86400000 - (now - data.timestamp))
+        return m.reply(`⚠️ Você já usou sua sorte hoje!\n\n⏳ Tente novamente em: *${restante}*`)
+    }
+
+    let sorte = Math.floor(Math.random() * 101)
     let numeros = Array.from({ length: 6 }, () => Math.floor(Math.random() * 10)).join('')
+
+    sorteData[user] = {
+        sorte,
+        numeros,
+        timestamp: now
+    }
+
+    fs.writeFileSync(file, JSON.stringify(sorteData))
 
     let msg = `🎰 *Sua Sorte de Hoje:*
     
@@ -13,7 +40,14 @@ _Talvez seja um bom dia pra tentar a sorte... ou não!_`
 }
 
 handler.help = ['sorte']
-handler.tags = ['fun']
+handler.tags = ['diversão', 'fun']
 handler.command = /^sorte$/i
 
 export default handler
+
+function clockString(ms) {
+    let h = Math.floor(ms / 3600000)
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
+}
